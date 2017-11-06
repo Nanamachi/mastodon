0f4e51f1aa459fc00144026aed39a01e7a684bb2
Nanamachi <town7.haruki@gmail.com>
2017-11-07 03:47:58 +0900

    default timeline hide unfederate statuses

--- app/models/status.rb
+++ app/models/status.rb
@@ -72,6 +72,7 @@ class Status < ApplicationRecord
   scope :tagged_with, ->(tag) { joins(:statuses_tags).where(statuses_tags: { tag_id: tag }) }
   scope :excluding_silenced_accounts, -> { left_outer_joins(:account).where(accounts: { silenced: false }) }
   scope :including_silenced_accounts, -> { left_outer_joins(:account).where(accounts: { silenced: true }) }
+  scope :excluding_unfederate, -> { where(federate: true) }
   scope :not_excluded_by_account, ->(account) { where.not(account_id: account.excluded_from_timeline_account_ids) }
   scope :not_domain_blocked_by_account, ->(account) { account.excluded_from_timeline_domains.blank? ? left_outer_joins(:account) : left_outer_joins(:account).where('accounts.domain IS NULL OR accounts.domain NOT IN (?)', account.excluded_from_timeline_domains) }
 
@@ -259,6 +260,7 @@ class Status < ApplicationRecord
 
     def filter_timeline_default(query)
       query.excluding_silenced_accounts
+      query.excluding_unfederate
     end
 
     def account_silencing_filter(account)
