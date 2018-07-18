bbf222d061ccbe979b46a7b114797eba73ea0def
Nanamachi <7machi@nanamachi.net>
2018-07-19 06:52:32 +0900

    fix services/request_approval_service: sent approval email to not only admin, but moderators

--- app/services/request_approval_service.rb
+++ app/services/request_approval_service.rb
@@ -7,8 +7,8 @@ class RequestApprovalService < BaseService
     end
 
     if Setting.require_approval
-      User.admins.each do |admin|
-        UserMailer.new_user_waiting_for_approval(admin, user).deliver_later
+      User.staff.each do |staff|
+        UserMailer.new_user_waiting_for_approval(staff, user).deliver_later
       end
     else
       user.approve
